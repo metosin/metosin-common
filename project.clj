6045b2185abe54acddf247a290987495f1482121
Matti Nieminen <matti.nieminen@metosin.fi>
2016-01-11 14:32:58 +0200

    Add project.clj created by lein new

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject metosin-common "0.1.0-SNAPSHOT"
+  :description "Random collection is various namespaces used in multiple Metosin projects."
+  :url "https://github.com/metosin/metosin-common"
+  :dependencies [[org.clojure/clojure "1.7.0"]])

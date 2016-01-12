4dceeb26a1edf7bb44d58cf0208ce9a4624543fd
Matti Nieminen <matti.nieminen@metosin.fi>
2016-01-12 12:03:32 +0200

    Update project.clj before publish

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject com.metosin/metosin-common "0.1.0-SNAPSHOT"
+(defproject metosin/metosin-common "0.1.0-SNAPSHOT"
   :description "Random collection is various namespaces used in multiple Metosin projects."
-  :license {:name "Proprietary"}
   :url "https://github.com/metosin/metosin-common"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
-  :plugins [[s3-wagon-private "1.2.0"]]
-  :repositories [["private" {:url "s3p://metosin-maven/releases/" :creds :gpg}]])
+  :source-paths ["src/clj" "src/cljc" "src/cljs"])

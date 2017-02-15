e140f1ea71999688423f45ba4103e9596e279883
Miikka Koskinen <miikka@metosin.fi>
2017-02-15 11:43:33 +0200

    Show subforms in `(is (not ...))` failure reports
    If `(is (pred ...))` fails, the arguments for `pred` are evaluated and
    displayed in the error message:
    
        boot.user> (def my-map {:a 1})
        boot.user> (is (contains? my-map :b))
    
        FAIL in () (boot.user6799508561646719539.clj:49)
        expected: (contains? my-map :b)
          actual: (not (contains? {:a 1} :b))
        false
    
    The same is not true for the relatively common `(is (not (pred ...)))` - all
    you get it is `actual: (not (not false))`. This patch implements a new
    `assert-expr` method to fix the situation:
    
        boot.user> (is (not (contains? my-map :a)))
    
        FAIL in () (boot.user6799508561646719539.clj:79)
        expected: (not (contains? my-map :a))
          actual: (contains? {:a 1} :a)
        true

---
+++ src/cljc/metosin/testing.clj
@@ -0,0 +1,26 @@
+(ns metosin.testing
+  (:require [clojure.test :refer [assert-any assert-expr do-report function?]]))
+
+;; assert-not and assert-expr are directly derived from clojure.test source code
+;; <https://github.com/clojure/clojure/blob/c0326d2386dd1227f35f46f1c75a8f87e2e93076/src/clj/clojure/test.clj>
+
+(defn- assert-not [msg form]
+  (let [subform (second form)
+        args (rest subform)
+        pred (first subform)]
+    `(let [values# (list ~@args)
+           result# (apply ~pred values#)]
+       (if-not result#
+         (do-report
+           {:type :pass, :message ~msg,
+            :expected '~form, :actual (list '~'not (cons '~pred values#))})
+         (do-report
+           {:type :fail, :message ~msg,
+            :expected '~form, :actual (cons '~pred values#)}))
+       result#)))
+
+(defmethod assert-expr 'not [msg form]
+  (let [subform (second form)]
+    (if (and (sequential? subform) (function? (first subform)))
+      (assert-not msg form)
+      (assert-any msg form))))

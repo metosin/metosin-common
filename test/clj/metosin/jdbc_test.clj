c6f1b63d8b6fdd517793e490734042455abd5f98
Juho Teperi <juho.teperi@iki.fi>
2017-05-23 16:38:20 +0300

    Smaller diff

--- test/clj/metosin/jdbc_test.clj
+++ test/clj/metosin/jdbc_test.clj
@@ -1,7 +1,7 @@
 (ns metosin.jdbc-test
-  (:require [metosin.jdbc :refer :all]
+  (:require [metosin.jdbc :refer [db-spec-defaults]]
             [clojure.test :refer :all]
-            [clojure.java.jdbc :as jdbc]
+            [clojure.java.jdbc :as jdbc :refer [query update! delete! execute! insert! insert-multi!]]
             [honeysql.core :as sql]))
 
 (def h2-spec {:classname "org.h2.Driver"
@@ -24,7 +24,7 @@
                db-spec-defaults
                ;; Select test DB
                (try
-                 (jdbc/query psql-spec ["select now()"])
+                 (query psql-spec ["select now()"])
                  (println "Running tests against Postgres")
                  psql-spec
                  (catch Exception e
@@ -45,65 +45,65 @@
     (testing "insert! with underscores"
       ;; Postgres insert returns the row, h2 doesn't
       (if (h2? db-spec)
-        (jdbc/insert! db :test_table {:test_column1 100})
+        (insert! db :test_table {:test_column1 100})
         (is (= [{:id 1 :test-column1 100 :test-column2 nil}]
-               (jdbc/insert! db :test_table {:test_column1 100})))))
+               (insert! db :test_table {:test_column1 100})))))
 
     (testing "query with underscores"
       (let [q {:select [:id :test_column1 :test_column2]
                :from [:test_table]}]
         (is (= (list {:id 1 :test-column1 100 :test-column2 nil})
-               (jdbc/query db (sql/format q))))))
+               (query db (sql/format q))))))
 
     (testing "insert! with dashes"
-      (jdbc/insert! db :test-table {:test-column2 "foo"}))
+      (insert! db :test-table {:test-column2 "foo"}))
 
     (testing "query with dashes"
       (let [q {:select [:id :test-column1 :test-column2]
                :from [:test_table]
                :where [:= :test-column2 "foo"]}]
         (is (= '({:id 2 :test-column1 nil :test-column2 "foo"})
-               (jdbc/query db (sql/format q))))))
+               (query db (sql/format q))))))
 
     (testing "update! with underscores"
-      (jdbc/update! db :test_table {:test_column1 200} ["id = ?" 1])
+      (update! db :test_table {:test_column1 200} ["id = ?" 1])
       (let [q {:select [:id :test-column1 :test-column2]
                :from [:test-table]
                :where [:= :id 1]}]
         (is (= '({:id 1 :test-column1 200 :test-column2 nil})
-               (jdbc/query db (sql/format q))))))
+               (query db (sql/format q))))))
 
     (testing "update! with dashes"
-      (jdbc/update! db :test-table {:test-column2 "bar"} ["id = ?" 1])
+      (update! db :test-table {:test-column2 "bar"} ["id = ?" 1])
       (let [q {:select [:id :test-column1 :test-column2]
                :from [:test-table]
                :where [:= :id 1]}]
         (is (= '({:id 1 :test-column1 200 :test-column2 "bar"})
-               (jdbc/query db (sql/format q))))))
+               (query db (sql/format q))))))
 
     (testing "delete! with underscores"
-      (jdbc/delete! db :test_table ["id = 1"])
+      (delete! db :test_table ["id = 1"])
       (let [q {:select [:*]
                :from [:test-table]
                :where [:= :id 1]}]
-        (is (= '() (jdbc/query db (sql/format q))))))
+        (is (= '() (query db (sql/format q))))))
 
     (testing "delete! with dashes"
-      (jdbc/delete! db :test-table ["id = 2"])
+      (delete! db :test-table ["id = 2"])
       (let [q {:select [:*]
                :from [:test-table]
                :where [:= :id 2]}]
-        (is (= '() (jdbc/query db (sql/format q))))))
+        (is (= '() (query db (sql/format q))))))
 
     (testing "insert-multi! with dashes"
       (if (h2? db-spec)
-        (jdbc/insert-multi! db :test-table [{:test-column2 "bar"} {:test-column2 "bar"}])
+        (insert-multi! db :test-table [{:test-column2 "bar"} {:test-column2 "bar"}])
         (is (= [{:id 3 :test-column1 nil :test-column2 "bar"} {:id 4 :test-column1 nil :test-column2 "bar"}]
-               (jdbc/insert-multi! db :test-table [{:test-column2 "bar"} {:test-column2 "bar"}]))))
+               (insert-multi! db :test-table [{:test-column2 "bar"} {:test-column2 "bar"}]))))
 
       (is (= [{:test-column2 "bar"} {:test-column2 "bar"}]
-             (jdbc/query db (sql/format {:select [:test-column2] :from [:test-table]})))))
+             (query db (sql/format {:select [:test-column2] :from [:test-table]})))))
 
     (testing "execute!"
       (is (= '(1)
-             (jdbc/execute! db ["INSERT INTO test_table (test_column1) VALUES (1)"]))))))
+             (execute! db ["INSERT INTO test_table (test_column1) VALUES (1)"]))))))
